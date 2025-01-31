02f3c31fb19beb55f9505a05ea020aa893e67397
wesfly <wesleydeanford@yahoo.com>
2025-01-31 11:04:00 +0100

    added notification timeout

--- days_until.sh
+++ days_until.sh
@@ -7,4 +7,4 @@ current_date=$(date +"%Y-%m-%d")
 # Calculate the difference in days
 days_until_target=$(( ($(date -d $target_date +%s) - $(date -d $current_date +%s)) / 86400 ))
 
-notify-send "Days Until March 1, 2025" "There are $days_until_target days left until March 1, 2025."
+notify-send -t 5000 "Days Until March 1, 2025" "There are $days_until_target days left until March 1, 2025."
