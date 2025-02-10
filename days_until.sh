e0f5c8157e616146fa87e98d5e8ddc767328f095
wesfly <175718527+wesfly@users.noreply.github.com>
2025-02-10 20:40:40 +0100

    added user input

--- days_until.sh
+++ days_until.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-target_date="2025-03-01"
+read -p "Enter date in YYYY-MM-DD format: " target_date
+# target_date="2025-03-01"
 
 current_date=$(date +"%Y-%m-%d")
 
 # Calculate the difference in days
 days_until_target=$(( ($(date -d $target_date +%s) - $(date -d $current_date +%s)) / 86400 ))
 
-notify-send -t 5000 "Days Until March 1, 2025" "There are $days_until_target days left until March 1, 2025."
+notify-send -t 5000 "Days Until March 1, 2025" "There are $days_until_target days left until $target_date."
